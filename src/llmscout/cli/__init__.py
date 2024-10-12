08f0668beb6a07eccde2409d6eba49ee1b23a77e
Cedric Vidal <cedric@vidal.biz>
2024-10-12 18:44:41 +0000

    Removed out of scope code

--- src/llmscout/cli/__init__.py
+++ src/llmscout/cli/__init__.py
@@ -1,9 +1,7 @@
 import rich_click as click
 import yaml
-from dotenv import load_dotenv
 from llmscout.__about__ import __version__
-from .scanner import do_scan_azure, export_proxy_endpoints
-from .azd_utils import load_azd_env
+from .scanner import do_scan_azure
 from .litellm_patched import run_server as litellm_server
 
 def llmscout():
@@ -16,20 +14,10 @@ def cli():
     pass
 
 @cli.command()
-@click.option('--scan-azure/--no-scan-azure', default=True, help='Whether to scan Azure searching for OpenAI endpoints.')
-@click.option('--set-azd-env/--no-set-azd-env', default=True, help='Set the selected deployment names as azd environment variables.')
-def scan(scan_azure, set_azd_env):
+def scan():
 
-    if scan_azure:
-        litellm_config = do_scan_azure()
+    litellm_config = do_scan_azure()
 
-        click.echo(f"Writing litellm_config.yaml")
-        with open('litellm_config.yaml', 'w') as f:
-            yaml.dump(litellm_config, f)
-
-    if set_azd_env:
-        load_azd_env()
-        load_dotenv(".env")
-        load_dotenv(".env.state")
-
-        export_proxy_endpoints()
+    click.echo(f"Writing litellm_config.yaml")
+    with open('litellm_config.yaml', 'w') as f:
+        yaml.dump(litellm_config, f)
--- src/llmscout/cli/scanner.py
+++ src/llmscout/cli/scanner.py
@@ -1,9 +1,6 @@
-import os
 import subprocess
 from itertools import groupby
-from .ai_config import *
 import json
-from pathlib import Path
 import click
 
 def list_resource(resource_type):
@@ -39,19 +36,6 @@ def get_oai_endpoint(account):
     account_properties = account['properties']
     account_endpoints = account_properties['endpoints']
     return account_endpoints['OpenAI Language Model Instance API'] if 'OpenAI Language Model Instance API' in account_endpoints else None
-
-def role_model_env_var_name(role):
-    return f'{role.upper()}_MODEL_NAME'
-
-def role_deployment_name_env_var_name(role):
-    return f'{role.upper()}_DEPLOYMENT_NAME'
-
-def read_env_role(role):
-    return {
-        "role": role,
-        "model_name": os.getenv(role_model_env_var_name(role)),
-        "deployment_name": os.getenv(role_deployment_name_env_var_name(role)),
-    }
 
 def get_az_ad_signed_in_user():
     result = subprocess.run(['az', 'ad', 'signed-in-user', 'show', "--query", "id"], capture_output=True, text=True)
@@ -160,44 +144,3 @@ def do_scan_azure():
             })
 
     return litellm_config
-
-def redact_secret(key, value):
-    """Redact a value from the logs if the key indicates that the value contains a keyword such as KEY or SECRET."""
-    if "KEY" in key or "SECRET" in key:
-        return value[:4] + "*" * (len(value) - 4)
-    return value
-
-def update_env_file(env_file, kv_tuples):
-    """Update the env file with the key and value."""
-    from pathlib import Path
-    from dotenv import dotenv_values
-
-    data = {}
-    if Path(env_file).exists() and Path(env_file).is_file():
-        data = dotenv_values(env_file)
-
-    for key, value in kv_tuples:
-        data[key] = value
-        click.echo(f"Updating env file {env_file} with {key}={redact_secret(key, value)}")
-
-    with open(env_file, "w") as f:
-        for k, v in data.items():
-            f.write(f"{k}={v}\n")
-
-def export_proxy_endpoints():
-    config = read_ai_config()
-    roles = get_roles(config.data)
-
-    env_values = []
-    for role_name in roles:
-        click.echo(f"Exporting env vars for role {role_name}")
-        role = read_env_role(role_name)
-        model_name = role['model_name']
-        deployment_name = role['deployment_name']
-        model_version = config.descriptors[deployment_name].model.version
-        model_id = f"{model_name}@{model_version}"
-        env_values.append((f"{role_name.upper()}_OPENAI_BASE_URL", "http://localhost:4000/"))
-        env_values.append((f"{role_name.upper()}_OPENAI_API_KEY", "DUMMY"))
-        env_values.append((f"{role_name.upper()}_OPENAI_DEPLOYMENT", model_id))
-
-    update_env_file(".env.state", env_values)
